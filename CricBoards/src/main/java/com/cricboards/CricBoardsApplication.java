3db6fe4432371da5062309a4e88b6e7660b00fef
tharakram <tharakram.ece@gmail.com>
2018-09-24 23:41:37 -0400

    Check-in v1.1

---
+++ CricBoards/src/main/java/com/cricboards/CricBoardsApplication.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package com.cricboards;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author thara
+ *
+ */
+@SpringBootApplication
+public class CricBoardsApplication {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		SpringApplication.run(CricBoardsApplication.class, args);
+	}
+
+}
